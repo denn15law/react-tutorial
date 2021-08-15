34eaf5f7f0fa1af45c97ae7d55eeec6217428ea9
dennis law <denn15_law@live.ca>
2021-08-15 02:17:38 -0400

    added reminder

--- src/App.js
+++ src/App.js
@@ -25,10 +25,22 @@ const App = () => {
     }
 ])
 
+const deleteTask = (id) => {
+  setTasks(tasks.filter((task) => task.id !== id ))
+}
+const toggleReminder = (id) => {
+  setTasks(
+    tasks.map((task) => task.id === id ? {...task, reminder: !task.reminder} : task)
+    )
+}
+
   return (
     <div className="container">
       <Header></Header>
-      <Tasks tasks = {tasks}></Tasks>
+      <Tasks 
+        tasks = {tasks} 
+        onDelete = {deleteTask}
+        onToggle = {toggleReminder}></Tasks>
     </div>
   )
 }
